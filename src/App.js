319960e6f03234bbc036b32686bf497efc22ff45
michelle-hey <miscmiche@gmail.com>
2020-03-23 19:47:33 -0400

    Fetched API data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import Axios from "axios";
 
 export default function App() {
+  const [characterInfo, setCharacterInfo] = useState([]);
+
+  useEffect(() => {
+    Axios
+      .get('http://rickandmortyapi.com/api/character/')
+      .then(res => {
+        setCharacterInfo(res.data.results)
+        console.log(res.data.results);
+      });
+  }, []);
+
+  useEffect(() => {
+
+  }, [characterInfo]);
+
+
+
   return (
     <main data-testid='app'>
       <Header />
